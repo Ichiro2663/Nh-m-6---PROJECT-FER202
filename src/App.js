d30bf8fd6e9493d01975432c62e4a63ede3bf963
Ichiro <153877195+Ichiro2663@users.noreply.github.com>
2025-03-21 10:19:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import ProductDetail from './components/ProductDetail';
 import SearchResultsPage from './components/SearchResultPage';
 import Login from './components/Login';
 import Register from './components/Register';
+import BlogPage from './components/BlogPage';
+import BlogDetail from './components/BlogDetail';
+import ProductList from './components/ProductList';
+
 function App() {
   return (
     <BrowserRouter>
@@ -12,9 +16,12 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductList />} />
         <Route path="/category/:id" element={<CategoryPage />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/search/:query" element={<SearchResultsPage />} />
+        <Route path="/blogs" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
         {/* Các route khác */}
       </Routes>
     </BrowserRouter>
