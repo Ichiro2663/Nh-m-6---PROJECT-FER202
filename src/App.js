d4b8425843e779a5a57f0113b076f5a7bc38d410
tinnvt-co <tinnvthe172346@fpt.edu.vn>
2025-03-21 13:17:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-    
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<HomePage />} />
